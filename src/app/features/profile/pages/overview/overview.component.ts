f1518269745adfeadcd620d036a7c096a03b4b3c
Hannes Schumacher <schumacher@creative-network.biz>
2021-02-11 14:49:19 +0100

    first profile overview

--- src/app/features/profile/pages/overview/overview.component.ts
+++ src/app/features/profile/pages/overview/overview.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -8,12 +9,25 @@ import { FieldTemplate } from '@model/fieldTemplate';
 import { isNonNull } from '@utilities';
 import { Observable } from 'rxjs';
 import { delay, filter, switchMap } from 'rxjs/operators';
+=======
+import { Component, OnInit } from '@angular/core';
+import { StoreService, UserService } from '@core/services';
+import { FieldTemplatesService } from '@core/services/data/field-templates.service';
+
+import { User, UserData } from '@model/auth';
+import { FieldTemplate } from '@model/fieldTemplate';
+import { Observable } from 'rxjs';
+import { switchMap, take } from 'rxjs/operators';
+
+
+>>>>>>> first profile overview
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
+<<<<<<< HEAD
 export class OverviewComponent implements OnInit, AfterViewInit{
   public fields: Observable<FieldTemplate[]>;
   public _currentTemplate: FieldTemplate;
@@ -47,6 +61,16 @@ export class OverviewComponent implements OnInit, AfterViewInit{
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+=======
+export class OverviewComponent implements OnInit{
+  public fields: Observable<FieldTemplate[]>
+  constructor(public readonly store:StoreService, public readonly fieldTemplateService: FieldTemplatesService) { 
+    
+  }
+  ngOnInit(): void {
+    this.fields = this.store.user$.pipe(switchMap((user)=> 
+      this.fieldTemplateService.getFields(user)))
+>>>>>>> first profile overview
   }
 
 }
