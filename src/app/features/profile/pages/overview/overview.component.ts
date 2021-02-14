4d2b006de2f2ade670e3f7c7aa4bfb9d9110f88e
Hannes Schumacher <schumacher@creative-network.biz>
2021-02-14 01:52:18 +0100

    adding mat-table to Profile Overview page

--- src/app/features/profile/pages/overview/overview.component.ts
+++ src/app/features/profile/pages/overview/overview.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -9,26 +8,15 @@ import { FieldTemplate } from '@model/fieldTemplate';
 import { isNonNull } from '@utilities';
 import { Observable } from 'rxjs';
 import { delay, filter, switchMap } from 'rxjs/operators';
-=======
-import { Component, OnInit } from '@angular/core';
-import { StoreService, UserService } from '@core/services';
-import { FieldTemplatesService } from '@core/services/data/field-templates.service';
 
-import { User, UserData } from '@model/auth';
-import { FieldTemplate } from '@model/fieldTemplate';
-import { Observable } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
-
-
->>>>>>> first profile overview
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-<<<<<<< HEAD
-export class OverviewComponent implements OnInit, AfterViewInit{
+
+export class OverviewComponent implements OnInit{
   public fields: Observable<FieldTemplate[]>;
   public _currentTemplate: FieldTemplate;
   public readonly name:String;
@@ -42,15 +30,12 @@ export class OverviewComponent implements OnInit, AfterViewInit{
   constructor(public readonly store:StoreService, public readonly fieldTemplateService: FieldTemplatesService) { 
     this.dataSource = new MatTableDataSource<FieldTemplate>();
   }
-  ngAfterViewInit(): void {
-    delay(3000);
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-    
-  }
-   ngOnInit(): void {
+  
+  ngOnInit(): void {
     this.store.user$.pipe(filter(user => isNonNull(user)),switchMap((user)=> this.fieldTemplateService.getFields(user)))
     .subscribe((fieldTemplates: FieldTemplate[])=> this.dataSource = new MatTableDataSource<FieldTemplate>(fieldTemplates))
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     
   }
 
@@ -61,16 +46,5 @@ export class OverviewComponent implements OnInit, AfterViewInit{
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
-=======
-export class OverviewComponent implements OnInit{
-  public fields: Observable<FieldTemplate[]>
-  constructor(public readonly store:StoreService, public readonly fieldTemplateService: FieldTemplatesService) { 
-    
   }
-  ngOnInit(): void {
-    this.fields = this.store.user$.pipe(switchMap((user)=> 
-      this.fieldTemplateService.getFields(user)))
->>>>>>> first profile overview
-  }
-
 }
